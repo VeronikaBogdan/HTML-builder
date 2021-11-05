817d115c97120e7c41c4f806386f8bd71bc60e51
Veronika <veron9399bog@gmail.com>
2021-11-06 01:54:18 +0300

    feat: do 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const readline = require('readline');
+const process = require('process');
+
+const { stdin: input, stdout: output } = require('process');
+
+const rl = readline.createInterface({ input, output });
+let stream = fs.createWriteStream('02-write-file/text.txt');
+
+console.log('Hello!\nInput the text');
+rl.on('line', (input) => {
+	if(input == 'exit') rl.close();
+	else {
+		stream.write(`${input} \n`);
+		console.log('Input the text');
+	}
+});
+
+process.on('exit', (code) => {
+  console.log('\nThanks! Bye.');
+});
