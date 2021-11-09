93edc39ecaafb13ad0bff1197c3932645edddc68
Veronika <veron9399bog@gmail.com>
2021-11-09 04:50:49 +0300

    feat: do 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,46 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 const stat = require('fs');
-// import { stat } from 'fs';
 
-// const pathsToCheck = ['./txtDir', './txtDir/file.txt'];
-
-// for (let i = 0; i < pathsToCheck.length; i++) {
-//   stat(pathsToCheck[i], (err, stats) => {
-//     console.log(stats.isDirectory());
-//     console.log(stats);
-//   });
-// }
 const pathToFolder = '03-files-in-folder/secret-folder';
-
 
 fs.readdir(pathToFolder, 
   { withFileTypes: true },
   (err, files) => {
-  console.log("\nCurrent directory files:");
   if (err)
     console.log(err);
   else {
     files.forEach((file) => {
 			if(file.isFile() == true) {
-				console.log(file);
-				console.log(file.name);
-				// fs.stat(path.join(pathToFolder, '/image.jpg'), (stats) => {
-				// 	// console.log(stats.isDirectory());
-				// 	console.log(file);
-				// });
-// // console.log(stats);
-// 				console.log(path.extname(file.name));
-
-// 				fs.stat(file, (stats) => {
-// 			    console.log(stats);
-// 				});
+				fs.stat(path.join(pathToFolder, `/${file.name}`), (err, stats) => {
+					console.log(`${file.name} - ${path.extname(file.name)} - ${Math.ceil(stats.size/1024)} kb`);
+				});
 			}
     })
-		console.log(path.join(pathToFolder, '/image.jpg'));
-		fs.stat(path.join(pathToFolder, '/image.jpg'), (stats) => {
-		// console.log(stats.isDirectory());
-		console.log(stats);
-		});
   }
 })
